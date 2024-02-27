ff60aca045d95eb6bd15b22f6609356ecbaa44d2
Alina Efimova <efimovaalina2011@gmail.com>
2024-02-27 09:24:50 +0000

    maps solution is done

--- main.go
+++ main.go
@@ -10,10 +10,12 @@ import (
 	"os"
 	"strings"
 
+	"github.com/jmoiron/sqlx"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// класс ответственный за отправление запросов в базу данных
+// класс ответственный за отправление запроса о заказах
 type OrderFetcher struct {
 	DB *sql.DB
 }
@@ -23,39 +25,113 @@ func NewOrderFetcher(db *sql.DB) *OrderFetcher {
 	return &OrderFetcher{DB: db}
 }
 
-// класс ответственный за вывод данных
-type ResultPrinter struct {
-	requiredItems [][]string
+// класс для хранения информации о заказанном продукте
+type OrderedItem struct {
+	OrderID      int
+	ItemQuantity int
 }
 
-// конструктор класса ResultPrinter
-func NewResultPrinter(requiredItems [][]string) *ResultPrinter {
-	return &ResultPrinter{requiredItems: requiredItems}
+// конструктор класса OrderedItem
+func NewOrderedItem(orderID, itemQuantity int) *OrderedItem {
+	return &OrderedItem{
+		OrderID:      orderID,
+		ItemQuantity: itemQuantity,
+	}
+}
+
+// класс для хранения информации о главном стеллаже
+type MainRack struct {
+	RackName string
+	ItemIDs  []int
+}
+
+// конструктор класса MainRack
+func NewMainRack(rackName string, itemIDs []int) *MainRack {
+	return &MainRack{
+		RackName: rackName,
+		ItemIDs:  itemIDs,
+	}
 }
 
 // функция выполняет запрос в бд и возвращает выборку данных о продуктах в заказе
-// в формате [[itemId, orderId, itemQuantity], [...]]
-func (of *OrderFetcher) fetchRequiredItems(orderIds []string) ([][]string, error) {
+// в формате map[itemId]: [OrderedItem1, OrderedItem2, ...]
+func (of *OrderFetcher) fetchRequiredItems(orderIds []string) (map[int][]OrderedItem, error) {
 	// валидация параметров
 	if len(orderIds) < 1 {
 		return nil, fmt.Errorf("необходимо ввести хоть один параметр")
 	}
-	query := `SELECT order_id, item_id, quantity FROM order_items WHERE order_id IN (` + strings.Repeat("?,", len(orderIds)-1) + `?) ORDER BY item_id;`
 
-	// создание структуры данных
-	var requiredItems [][]string
+	query, args, err := sqlx.In("SELECT order_id, item_id, quantity FROM order_items WHERE order_id IN (?)", orderIds)
+	if err != nil {
+		return nil, err
+	}
+	// создание структуры данных для хранения результатов
+	orderedItemsMap := make(map[int][]OrderedItem)
 
-	// подготовка запроса
 	q, err := of.DB.Prepare(query)
 	if err != nil {
 		return nil, err
 	}
 	defer q.Close()
 
-	args := make([]interface{}, len(orderIds))
-	for i, v := range orderIds {
-		args[i] = v
+	// отправка запроса в бд
+	rows, err := q.Query(args...)
+	if err != nil {
+		return nil, err
 	}
+	defer rows.Close()
+
+	// обработка результатов запроса
+	for rows.Next() {
+		var orderId, itemId, itemQuantity int
+		err := rows.Scan(&orderId, &itemId, &itemQuantity)
+		if err != nil {
+			return nil, err
+		}
+
+		var data []OrderedItem
+
+		existingData, exists := orderedItemsMap[itemId]
+		if !exists {
+			// если объект еще не в мапе, создаем новый объект класса OrderedItem
+			data = []OrderedItem{}
+		} else {
+			data = existingData
+		}
+
+		newOrderedItem := NewOrderedItem(orderId, itemQuantity)
+		data = append(data, *newOrderedItem)
+
+		orderedItemsMap[itemId] = data
+	}
+
+	// проверка на наличие ошибок при обработке результатов
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return orderedItemsMap, nil
+}
+
+// функция выполняет запрос в бд и возвращает выборку данных о продуктах в заказе
+// в формате map[itemId]: itemName
+func (of *OrderFetcher) fetchItemName(itemIds []int) (map[int]string, error) {
+	// валидация параметров
+	if len(itemIds) < 1 {
+		return nil, fmt.Errorf("необходимо ввести хоть один параметр")
+	}
+
+	query, args, err := sqlx.In("SELECT id, name FROM items WHERE id IN (?)", itemIds)
+	if err != nil {
+		return nil, err
+	}
+	// создание структуры данных для хранения результатов
+	itemInfoMap := make(map[int]string)
+
+	q, err := of.DB.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+	defer q.Close()
 
 	// отправка запроса в бд
 	rows, err := q.Query(args...)
@@ -66,195 +142,92 @@ func (of *OrderFetcher) fetchRequiredItems(orderIds []string) ([][]string, error
 
 	// обработка результатов
 	for rows.Next() {
-		var orderId, itemId, itemQuantity string
-		err := rows.Scan(&orderId, &itemId, &itemQuantity)
-		if err != nil {
-			return nil, err
-		}
-		requiredItems = append(requiredItems, []string{itemId, orderId, itemQuantity})
-	}
-
-	// проверка на наличие ошибок при обработке результатов
-	if err := rows.Err(); err != nil {
-		return nil, err
-	}
-	return requiredItems, nil
-}
-
-// функция выполняет запрос в бд, дополняет существующую выборку новыми данными и
-// возвращает выборку данных о продуктах в заказе
-// в формате [[itemId, orderId, itemQuantity, itemName], [...]]
-func (of *OrderFetcher) fetchItemNames(itemIds []string, requiredItems [][]string) ([][]string, error) {
-	// валидация параметров
-	if len(itemIds) < 1 {
-		return nil, fmt.Errorf("необходимо ввести хоть один параметр")
-	}
-	// подготовка запроса
-	query1 := `SELECT id, name FROM items WHERE id IN (` + strings.Repeat("?,", len(itemIds)-1) + `?);`
-
-	// создание структуры данных для временного хранения результатов
-	itemNameMap := make(map[string]string)
-
-	// подготовка параметров
-	q, err := of.DB.Prepare(query1)
-	if err != nil {
-		return nil, err
-	}
-	defer q.Close()
-	args := make([]interface{}, len(itemIds))
-	for i, v := range itemIds {
-		args[i] = v
-	}
-
-	// отправка запроса в бд
-	rows, err := q.Query(args...)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	// обработка результатов запроса
-	for rows.Next() {
-		var itemId, itemName string
+		var itemId int
+		var itemName string
 		err := rows.Scan(&itemId, &itemName)
 		if err != nil {
 			return nil, err
 		}
-		_, exists := itemNameMap[itemId]
+		_, exists := itemInfoMap[itemId]
 		if !exists {
-			itemNameMap[itemId] = itemName
+			itemInfoMap[itemId] = itemName
 		}
 	}
-	// добавление данных о названии продукта
-	for i, values := range requiredItems {
-		if itemName, exists := itemNameMap[values[0]]; exists {
-			requiredItems[i] = append(values, itemName)
-		}
-	}
-	return requiredItems, nil
-}
-
-// функция выполняет запрос в бд, дополняет существующую выборку новыми данными и
-// возвращает выборку данных о продуктах в заказе
-// в формате [[itemId, orderId, itemQuantity, itemName, itemSubRack1 itemSubRack2..., itemMainRack], [...]]
-func (of *OrderFetcher) fetchItemsRacks(itemIds []string, requiredItems [][]string) ([][]string, error) {
-	// валидация параметров
-	if len(itemIds) < 1 {
-		return nil, fmt.Errorf("необходимо ввести хоть один параметр")
-	}
-
-	// создание структуры данных для временного хранения результатов
-	itemSubRackMap := make(map[string]string)
-
-	// подготовка первого запроса (доп стеллажи)
-	query1 := `SELECT rack_id, item_id, rack_name FROM item_rack WHERE is_main = FALSE AND item_id IN (` + strings.Repeat("?,", len(itemIds)-1) + `?) ORDER BY rack_id;`
-
-	q, err := of.DB.Prepare(query1)
-	if err != nil {
+	// проверка на наличие ошибок при обработке результатов
+	if err := rows.Err(); err != nil {
 		return nil, err
 	}
-	defer q.Close()
+	return itemInfoMap, nil
+}
 
-	defer q.Close()
-	args := make([]interface{}, len(itemIds))
-	for i, v := range itemIds {
-		args[i] = v
+// функция выполняет запрос в бд и возвращает две структуры данных с результатами:
+// 1) map[rackId]:MainRack1 для хранения результатов о главных стеллажах
+// 2) map[itemId]:subRackName1, subRackName2...(concatenated string) для
+// хранения результатов о доп стеллажах
+func (of *OrderFetcher) fetchRackInfo(itemIds []int) (map[int]MainRack, map[int]string, error) {
+	// валидация параметров
+	if len(itemIds) < 1 {
+		return nil, nil, fmt.Errorf("необходимо ввести хоть один параметр")
 	}
+
+	query, args, err := sqlx.In("SELECT rack_id, rack_name, item_id, is_main FROM item_rack WHERE item_id IN (?)", itemIds)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// создание структур данных для хранения результатов
+	mainRackItemMap := make(map[int]MainRack)
+	subRackNameMap := make(map[int]string)
+
+	q, err := of.DB.Prepare(query)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer q.Close()
 
 	// отправка запроса в бд
 	rows, err := q.Query(args...)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	defer rows.Close()
 
-	// обработка результатов запроса
 	for rows.Next() {
-		var itemId, rackId, rackName string
-		err := rows.Scan(&rackId, &itemId, &rackName)
+		var rackId, itemId int
+		var rackName string
+		var isMain bool
+		err := rows.Scan(&rackId, &rackName, &itemId, &isMain)
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
-		if existingRack, exists := itemSubRackMap[itemId]; exists {
-			itemSubRackMap[itemId] = existingRack + "," + rackName
+		// если главный стеллаж, сохраняем в одну структуру данных
+		if isMain {
+			existingData, exists := mainRackItemMap[rackId]
+			if !exists {
+				existingData = MainRack{
+					RackName: rackName,
+					ItemIDs:  []int{itemId},
+				}
+			} else {
+				existingData.ItemIDs = append(existingData.ItemIDs, itemId)
+			}
+
+			mainRackItemMap[rackId] = existingData
 		} else {
-			itemSubRackMap[itemId] = rackName
+			// доп стеллажи связываем с itemId для удобства вывода
+			existingValue, exists := subRackNameMap[itemId]
+			if exists {
+				subRackNameMap[itemId] = existingValue + "," + rackName
+			} else {
+				subRackNameMap[itemId] = rackName
+			}
 		}
 	}
-
-	// добавление данных о названиях дополнительных стеллажей
-	for i, values := range requiredItems {
-		// если доп стеллажей нет, место займет "0"
-		requiredItems[i] = append(values, "0")
-		if rackName, exists := itemSubRackMap[values[0]]; exists {
-			requiredItems[i] = append(values, rackName)
-		}
+	// проверка на наличие ошибок при обработке результатов
+	if err := rows.Err(); err != nil {
+		return nil, nil, err
 	}
-
-	// подготовка второго запроса (главные стеллажи)
-	query2 := `SELECT rack_id, item_id, rack_name FROM item_rack WHERE is_main = TRUE AND item_id IN (` + strings.Repeat("?,", len(itemIds)-1) + `?) ORDER BY rack_name;`
-
-	// создание структуры данных для временного хранения результатов
-	itemRackMap := make(map[string]string)
-
-	// подготовка запроса
-	q, err = of.DB.Prepare(query2)
-	if err != nil {
-		return nil, err
-	}
-	defer q.Close()
-
-	defer q.Close()
-	for i, v := range itemIds {
-		args[i] = v
-	}
-
-	// отправка запроса в бд
-	rows, err = q.Query(args...)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	// обработка результатов запроса
-	for rows.Next() {
-		var itemId, rackId, rackName string
-		err := rows.Scan(&rackId, &itemId, &rackName)
-		if err != nil {
-			return nil, err
-		}
-		if existingRack, exists := itemRackMap[itemId]; exists {
-			itemRackMap[itemId] = existingRack + "," + rackName
-		} else {
-			itemRackMap[itemId] = rackName
-		}
-	}
-	// добавление данных о названиях главных стеллажей
-	for i, values := range requiredItems {
-		if rackName, exists := itemRackMap[values[0]]; exists {
-			requiredItems[i] = append(values, rackName)
-		}
-	}
-	return requiredItems, nil
-}
-
-// функция печатает конечный результат на основе структуры данных в формате
-// [[itemId, orderId, itemQuantity, itemName, itemSubRack1 itemSubRack2..., itemMainRack], [...]]
-func (of *ResultPrinter) printResult() error {
-	currRack := ""
-	for _, values := range of.requiredItems {
-		if currRack != values[len(values)-1] {
-			fmt.Printf("===== Стеллаж %v:\n", values[len(values)-1])
-		}
-		currRack = values[len(values)-1]
-		fmt.Printf("%v (id=%v)\n", values[3], values[0])
-		fmt.Printf("заказ %v, %v шт\n", values[1], values[2])
-		if values[4] != "0" {
-			fmt.Printf("доп. стеллаж: %v\n", values[4])
-		}
-		println()
-	}
-	return nil
+	return mainRackItemMap, subRackNameMap, nil
 }
 
 func main() {
@@ -267,7 +240,7 @@ func main() {
 	// парсинг параметров командной строки
 	orderIds := strings.Split(os.Args[1], ",")
 
-	// соединение с базой данных
+	// сокдинение с базой данных
 	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/go_challenge")
 	if err != nil {
 		log.Fatal(err)
@@ -280,43 +253,48 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// создание объекта класса orderFetcher
+	// создание объекта класса OrderFetcher
 	orderFetcher := NewOrderFetcher(db)
 
-	// вызов метода FetchRequiredItems для выполнения запроса и хранения результатов
-	requiredItems, err := orderFetcher.fetchRequiredItems(orderIds)
+	// вызов метода fetchRequiredItems для выполнения запроса и хранения результатов
+	orderedItemsMap, err := orderFetcher.fetchRequiredItems(orderIds)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// вызов метода FetchItemNames для выполнения запроса и дополнения структуры данных
+	// вызов метода fetchItemName для выполнения запроса и хранения результатов
 	// подготовка параметров
-	var itemIDs []string
-	for _, values := range requiredItems {
-		if len(values) > 0 {
-			itemIDs = append(itemIDs, values[0])
-		}
+	var itemIds []int
+
+	for key := range orderedItemsMap {
+		itemIds = append(itemIds, key)
 	}
-	requiredItems, err = orderFetcher.fetchItemNames(itemIDs, requiredItems)
+	itemNameMap, err := orderFetcher.fetchItemName(itemIds)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// вызов метода FetchItemRacks для выполнения запроса и дополнения структуры данных
-	requiredItems, err = orderFetcher.fetchItemsRacks(itemIDs, requiredItems)
+	// вызов метода fetchRackInfo для выполнения запроса и хранения результатов
+	mainRackItemMap, subRackNameMap, err := orderFetcher.fetchRackInfo(itemIds)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// начало вывода
-	// создание объекта класса ResultPrinter
-	resultPrinter := NewResultPrinter(requiredItems)
-
-	// вывод заголовка
-	fmt.Printf("=+=+=+=\nСтраница сборки заказов %s\n", os.Args[1])
-	// вывод данных
-	err = resultPrinter.printResult()
-	if err != nil {
-		log.Fatal(err)
+	fmt.Printf("=+=+=+= Страница сборки заказов %s\n", os.Args[1])
+	for _, mainRack := range mainRackItemMap {
+		fmt.Printf("=====Стеллаж %v:\n", mainRack.RackName)
+		for _, itemId := range mainRack.ItemIDs {
+			itemName := itemNameMap[itemId]
+			for _, orderedItem := range orderedItemsMap[itemId] {
+				itemInOrder := orderedItem.OrderID
+				itemQuantityInOrder := orderedItem.ItemQuantity
+				itemSubRack, exists := subRackNameMap[itemId]
+				fmt.Printf("%v (id=%v)\nзаказ %v, %v шт \n", itemName, itemId, itemInOrder, itemQuantityInOrder)
+				if exists {
+					fmt.Printf("доп стеллаж: %v\n", itemSubRack)
+				}
+				fmt.Println()
+			}
+		}
 	}
 }
